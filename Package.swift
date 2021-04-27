e7d9326b31585582746fbe9b325baef7e4426ae5
Adam Fowler <adamfowler71@gmail.com>
2021-04-27 13:56:36 +0100

    Replace custom async code with _NIOConcurrency versions

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.16.1")),
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.28.0")),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.7.2")),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.3.0")),
@@ -45,6 +45,7 @@ let package = Package(
                 .product(name: "NIOSSL", package: "swift-nio-ssl"),
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
                 .product(name: "NIOFoundationCompat", package: "swift-nio"),
+                .product(name: "_NIOConcurrency", package: "swift-nio"),
             ]
         ),
         .target(name: "SotoCrypto", dependencies: []),
